d133b25fc02eee2f792c528fe4856b54dd10f891
saadProf <saadprof101@gmail.com>
2023-10-14 18:50:36 +0600

    tool cards dynamiced by DOM.

--- script.js
+++ script.js
@@ -13,35 +13,34 @@ const displayData = (tools) =>{
     const cardsContainer = document.getElementById("cards-container");
 
     tools.forEach(tool =>{
-        // console.log(tool);
+        console.log(tool);
 
         const cardDiv = document.createElement("div");
         cardDiv.classList = "bg-white drop-shadow-lg border rounded-lg";
         cardDiv.innerHTML = `
             <div class="p-4">
                 <div>
-                    <img class="mx-auto rounded-lg" src="chatgpt_assistente.jpg" alt="Ai tool image" />
+                    <img class="mx-auto rounded-lg" src="${tool.image}" alt="Ai tool image" />
                 </div>
                 <h3 class="text-2xl font-semibold mt-3">Features</h3>
-                <ol class="list-decimal ml-4">
-                    <li>Hi</li>
-                    <li>Hello</li>
-                    <li>Hola!</li>
+                <ol id="features-container" class="list-decimal ml-4">
+                    
                 </ol>
                 <hr class="my-3" />
 
                 <div class="flex items-center justify-between">
                     <div>
-                        <h3 class="text-2xl font-semibold mb-1">Tools Name</h3>
+                        <h3 class="text-2xl font-semibold mb-1">${tool.name}</h3>
                         <div class="flex items-center gap-2">
                             <i class="fa-solid fa-calendar-days opacity-70"></i>
-                            <p>11/02/2022</p>
+                            <p>${tool.published_in}</p>
                         </div>
                     </div>
                     <i class="fa-solid fa-arrow-right p-3 bg-sky-100 text-sky-500 rounded-full cursor-pointer"></i>
                 </div>
             </div>
         `;
+
         cardsContainer.appendChild(cardDiv);
     });
 }
