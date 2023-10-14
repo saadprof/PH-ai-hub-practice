3d310776120b9051a5966e9e3ab93989571821e1
saadProf <saadprof101@gmail.com>
2023-10-14 18:28:58 +0600

    Display data from api function implementd.

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const loadTool = async () =>{
     const res = await fetch(`https://openapi.programming-hero.com/api/ai/tools`);
     const data = await res.json();
     const tools = data.data.tools;
+    displayData(tools);
+}
+
+// Display data
+const displayData = (tools) =>{
+    // console.log(tools);
+
+    
 }
 
 
